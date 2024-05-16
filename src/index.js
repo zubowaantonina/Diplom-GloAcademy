fc2d6cf5f572ed6db4641bfa6be72a86f2a3cc71
zubowaantonina <zubowaantonina@yandex.ru>
2024-05-16 20:48:50 +0300

    timer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import scroll from "./modules/scroll.js";
 import servicesModal from "./modules/servicesModal.js";
 import modalRequest from "./modules/modalRequest.js";
+import timer from "./modules/timer.js";
 scroll();
 servicesModal();
 modalRequest();
+timer('21 may 2024');
---
+++ src/modules/timer.js
@@ -0,0 +1,55 @@
+const timer = (deadline) => {
+    const orderBlocks = document.querySelectorAll(".order");
+    const padZero = (elem) => {
+      if (elem < 10) {
+        return (elem = `0${elem}`);
+      } else {
+        return elem;
+      }
+    };
+    const getTimeRemaining = () => {
+      let dateStop = new Date(deadline).getTime();
+      let dateNow = new Date().getTime();
+      let timeRemaining = (dateStop - dateNow) / 1000;
+      let days = Math.floor(timeRemaining / 3600 / 24);
+      let hours = Math.floor((timeRemaining / 3600) % 24);
+      let minutes = Math.floor((timeRemaining / 60) % 60);
+      let seconds = Math.floor(timeRemaining % 60);
+  
+      seconds = padZero(seconds);
+      minutes = padZero(minutes);
+      hours = padZero(hours);
+      days = padZero(days);
+      console.log(days);
+      return { timeRemaining, days, hours, minutes, seconds };
+    };
+  
+    orderBlocks.forEach((block) => {
+      const timerDays = block.querySelector(".count.count_1 > span");
+      const timerHours = block.querySelector(".count.count_2 > span");
+      const timerMinutes = block.querySelector(".count.count_3 > span");
+      const timerSeconds = block.querySelector(".count.count_4 > span");
+      let intervalId;
+      const updateClock = () => {
+        let getTime = getTimeRemaining();
+  
+        timerDays.textContent = getTime.days;
+        timerHours.textContent = getTime.hours;
+        timerMinutes.textContent = getTime.minutes;
+        timerSeconds.textContent = getTime.seconds;
+  
+        if (getTime.timeRemaining <= 0) {
+          timerDays.textContent = "00";
+          timerHours.textContent = "00";
+          timerMinutes.textContent = "00";
+          timerSeconds.textContent = "00";
+          clearInterval(intervalId);
+        }
+      };
+  
+      intervalId = setInterval(updateClock, 500);
+      updateClock();
+    });
+  };
+  export default timer;
+  
