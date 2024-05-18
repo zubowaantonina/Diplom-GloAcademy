2e469530f3f73ea1ddf25adb9918b5307c653c87
zubowaantonina <zubowaantonina@yandex.ru>
2024-05-18 23:00:50 +0300

    slider

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,4 +1,39 @@
-const slider=()=>{
+const slider = () => {
+  const slides = document.querySelectorAll(".service-item");
+  const arrowLeft = document.getElementById("arrow-left");
+  const arrowRight = document.getElementById("arrow-right");
+  let mediaQuery = window.matchMedia("(min-width: 576px)");
 
-}
-export default slider
+  let currentSlide = 0;
+
+  let prevSlide = (elems, index, strClass) => {
+    elems[index].classList.remove(strClass);
+    elems[index + 1].classList.remove(strClass);
+  };
+  let nextSlide = (elems, index, strClass) => {
+    elems[index].classList.add(strClass);
+    elems[index + 1].classList.add(strClass);
+  };
+
+  arrowRight.addEventListener("click", () => {
+    prevSlide(slides, currentSlide, "service-item-active");
+    currentSlide++;
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+      currentSlide = 0;
+    }
+    nextSlide(slides, currentSlide, "service-item-active");
+  });
+  arrowLeft.addEventListener("click", () => {
+    prevSlide(slides, currentSlide, "service-item-active");
+
+    currentSlide--;
+    currentSlide--;
+    if (currentSlide < 0) {
+      currentSlide = slides.length - 2;
+    }
+
+    nextSlide(slides, currentSlide, "service-item-active");
+  });
+};
+export default slider;
