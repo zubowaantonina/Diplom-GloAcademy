8710cb34fe12ebd72a6da93ffa6de27bb400520b
zubowaantonina <zubowaantonina@yandex.ru>
2024-05-26 19:52:40 +0300

    optimization slider

--- src/modules/sliderSplade.js
+++ src/modules/sliderSplade.js
@@ -1,17 +1,20 @@
 import Splide from "@splidejs/splide";
+
+Splide.defaults = {
+  pagination: false,
+  arrows: false,
+  breakpoints: {
+    576: {
+      perPage: 1,
+    }
+  }
+}
+
 export const benefitsSplide = () => {
   const left = document.querySelector(".benefits__arrow--left");
   const right = document.querySelector(".benefits__arrow--right");
-
   const spide = new Splide(".benefits-splide", {
-    pagination: false,
-    arrows: false,
     perPage: 3,
-    breakpoints: {
-      576: {
-        perPage: 1,
-      },
-    },
   }).mount();
   left.addEventListener("click", (e) => {
     spide.go('-1')
@@ -25,14 +28,7 @@ export const serviceSplide=()=>{
   const left = document.querySelector(".services__arrow--left");
   const right = document.querySelector(".services__arrow--right");
   const spider = new Splide(".service-splide", {
-    pagination: false,
-    arrows: false,
     perPage: 2,
-    breakpoints: {
-      576: {
-        perPage: 1,
-      },
-    },
   }).mount();
   left.addEventListener("click", (e) => {
     spider.go('-1')
