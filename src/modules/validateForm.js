97580d4e947e3f1db8b5f37e1c63e8622412a90c
zubowaantonina <zubowaantonina@yandex.ru>
2024-05-26 22:15:37 +0300

    fix

--- src/modules/validateForm.js
+++ src/modules/validateForm.js
@@ -1,26 +1,30 @@
 const validateForm = () => {
-    const body = document.querySelector(".balkony");
-    const nameInputs = document.querySelectorAll('input[name="fio"]');
-    const phoneInputs = document.querySelectorAll('input[name="phone"]');
-    const calcInputs = document.getElementById("calc-input");
-  
-    nameInputs.forEach((input) => {
-      input.addEventListener("input", (e) => {
-        e.target.value = e.target.value.replace(/[^а-яА-Яa-zA-Z ]/g, "");
-      });
+  const body = document.querySelector(".balkony");
+  const userNames = document.querySelectorAll('input[name="fio"]');
+  const userPhones = document.querySelectorAll('input[name="phone"]');
+  const calcInputs = document.getElementById("calc-input");
+
+  userNames.forEach((input) => {
+    input.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^а-яА-Яa-zA-Z ]/g, "");
     });
-  
-    phoneInputs.forEach((input) => {
-      input.addEventListener("input", (e) => {
-        e.target.value = e.target.value.replace(/[^0-9()+]/g, "");
-      });
+  });
+  const re = /^\d[\d\(\)\ -]{7,14}\d$/;
+  userPhones.forEach((input) => {
+    input.addEventListener("input", (e) => {
+      const valid = re.test(e.target.value);
+      if (!valid) {
+        input.classList.add("error");
+      } else {
+        input.classList.remove("error");
+      }
     });
-    
-    if (body) {
-      calcInputs.addEventListener("input", (e) => {
-        e.target.value = e.target.value.replace(/[^0-9]/g, "");
-      });
-    }
-  };
-  export default validateForm;
-  
+  });
+
+  if (body) {
+    calcInputs.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^0-9]/g, "");
+    });
+  }
+};
+export default validateForm;
