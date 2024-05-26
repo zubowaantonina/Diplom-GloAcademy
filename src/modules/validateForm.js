bb6ddbe22952a7a218672a69eda05a1071d2a6b5
zubowaantonina <zubowaantonina@yandex.ru>
2024-05-26 18:41:49 +0300

    fix sendForm

--- src/modules/validateForm.js
+++ src/modules/validateForm.js
@@ -1,21 +1,26 @@
 const validateForm = () => {
   const body = document.querySelector(".balkony");
-  const nameInputs = document.querySelectorAll('input[name="fio"]');
-  const phoneInputs = document.querySelectorAll('input[name="phone"]');
+  const userNames = document.querySelectorAll('input[name="fio"]');
+  const userPhones = document.querySelectorAll('input[name="phone"]');
   const calcInputs = document.getElementById("calc-input");
-
-  nameInputs.forEach((input) => {
+  
+  userNames.forEach((input) => {
     input.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^а-яА-Яa-zA-Z ]/g, "");
     });
   });
-
-  phoneInputs.forEach((input) => {
+  const re = /^\d[\d\(\)\ -]{7,16}\d$/;
+  userPhones.forEach((input) => {
     input.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(/[^0-9()+]/g, "");
+      const valid = re.test(e.target.value);
+      if (!valid) {
+        input.classList.add("error");
+      } else {
+        input.classList.remove("error");
+      }
     });
   });
-
+  
   if (body) {
     calcInputs.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^0-9]/g, "");
