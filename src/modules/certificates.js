953675348391ea718fb473268d94f587157ff08b
zubowaantonina <zubowaantonina@yandex.ru>
2024-05-26 16:01:53 +0300

    fix certificates

--- src/modules/certificates.js
+++ src/modules/certificates.js
@@ -1,22 +1,23 @@
 const certificates = () => {
   const docImages = document.querySelectorAll(".sertificate-document"); //a
+
+  const modalDoc = document.createElement("div");
+  modalDoc.innerHTML = `
+  <div class="document-modal-content"><img src="images/documents/original/document4.jpg" alt="" ></div>
+  `;
+  modalDoc.classList.add("document-modal");
+  document.body.appendChild(modalDoc);
+
   const closeModal = (e) => {
     const modalImg = document.querySelector(".document-modal-content>img");
     if (e.target !== modalImg) {
       modalDoc.classList.remove("is-open");
     }
   };
-  const modalDoc = document.createElement("div");
-  modalDoc.classList.add("document-modal");
-  modalDoc.innerHTML = `
-  <div class="document-modal-content"><img src="images/documents/original/document4.jpg" alt="" ></div>
-  `;
-  document.body.appendChild(modalDoc);
-  modalDoc.addEventListener("click", closeModal);
-
   const open = () => {
     modalDoc.classList.add("is-open");
   };
+
   if (document.querySelector(".sertificate-document")) {
     const docItems = document.querySelectorAll(".documents_item");
     docItems.forEach((doc) => {
@@ -30,5 +31,6 @@ const certificates = () => {
       });
     });
   }
+  modalDoc.addEventListener("click", closeModal);
 };
 export default certificates;
